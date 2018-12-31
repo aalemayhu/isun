6e0fd32426a23da0e9ad5eba1fbefb18480c1a83
Alexander Alemayhu <alexander@alemayhu.com>
2018-12-31 02:52:48 +0100

    format file
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var axios = require("axios");
 
 exports.IsUsingNetlify = function(url, cb) {
-  axios(url, { method: 'HEAD', mode: 'no-cors' }).then( (res) => {
+  axios(url, { method: "HEAD", mode: "no-cors" }).then( (res) => {
     if (res.headers.server.includes("Netlify")) {
       cb(true);
     } else {
